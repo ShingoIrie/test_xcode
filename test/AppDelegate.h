760032089e9c3dd1689573784cbcc9af2b995e3f
ShingoIrie <shingo@plus.vc>
2013-12-25 15:03:27 +0900

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Irie Shingo on 2013/12/25.
+//  Copyright (c) 2013年 PLUS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Irie Shingo on 2013/12/25.
+//  Copyright (c) 2013年 PLUS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
